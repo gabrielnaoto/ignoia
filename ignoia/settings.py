ca06e2c256c573ee06c58533b2881db23ffbe0d1
Gabriel Naoto <gabrielnaoto@yahoo.com.br>
2018-09-18 17:55:20 -0300

    1

--- ignoia/settings.py
+++ ignoia/settings.py
@@ -135,5 +135,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'naoto.ymai@gmail.com'
-EMAIL_HOST_PASSWORD = 'naotoymai153'
+EMAIL_HOST_PASSWORD = '1'
 EMAIL_USE_TLS = True
