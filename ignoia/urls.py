679f1944faaa5b6504361ce829da24ba409e3d5d
Gabriel Naoto <gabrielnaoto@yahoo.com.br>
2018-09-19 00:30:32 -0300

    Últimos ajustes

--- ignoia/urls.py
+++ ignoia/urls.py
@@ -14,13 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
 from inscricao import views
 from inscricao.views import CreateInscricao, comprovante
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('inscricao/', CreateInscricao.as_view(), name='inscricao'),
     path('obrigado/', comprovante, name='comprovante'),
--- inscricao/models.py
+++ inscricao/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-# Create your models here.
 from inscricao.validators import validate_CPF
 
 
@@ -14,7 +13,9 @@ class Curso(models.Model):
 class Inscricao(models.Model):
     cpf = models.CharField(max_length=14, unique=True, verbose_name='CPF', validators=[validate_CPF])
     nome = models.CharField(max_length=100)
-    email = models.EmailField(verbose_name='E-mail', help_text='Através deste endereço eletrônico você receberá sua confirmação de inscrição')
     data_nascimento = models.DateField(blank=True, null=True, verbose_name='Data de nascimento')
     telefone = models.CharField(max_length=25, help_text='Exemplo: (XX) XXXXX-XXXX')
-    curso = models.ForeignKey(Curso, verbose_name='Curso de graduação', blank=True, null=True, on_delete=models.DO_NOTHING, help_text='Selecione um curso da lista')
+    curso = models.ForeignKey(Curso, verbose_name='Curso de graduação', blank=True, null=True,
+                              on_delete=models.DO_NOTHING, help_text='Selecione um curso da lista')
+    email = models.EmailField(verbose_name='E-mail',
+                              help_text='Através deste endereço eletrônico você receberá sua confirmação de inscrição')
